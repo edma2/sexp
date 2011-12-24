229b0dde15665bf25457a630a6e2d88cdec18775
Eugene Ma <edma2@san.rr.com>
2011-12-24 14:13:02 -0800

    added license

--- sexp.c
+++ sexp.c
@@ -1,3 +1,23 @@
+/*
+ * sexp - Scheme interpreter
+ * Version 1.0 - December 2011
+ * Written by Eugene D. Ma - <edma12123@gmail.com>
+ *
+ * Copyright (C) 2009-2011 Eugene D. Ma
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
