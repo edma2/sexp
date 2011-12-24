3cbad2af190b79c3e615ae70d6f5a830cae26c9c
Eugene Ma <edma2@san.rr.com>
2011-12-23 19:49:17 -0800

    implemented empty? in sample

--- sample.scm
+++ sample.scm
@@ -1,3 +1,4 @@
-(define length (lambda (ls) 
-                 (if (eq? ls '()) 0 
+(define empty? (lambda (x) (eq? x '())))
+(define length (lambda (ls)
+                 (if (empty? ls) 0
                    (+ 1 (length (cdr ls))))) 
