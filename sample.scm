e3d2f15c026ccf4751a99b7489cbbba6b7963f60
Eugene Ma <edma2@san.rr.com>
2011-12-23 19:56:42 -0800

    added fib to sample.scm

--- sample.scm
+++ sample.scm
@@ -1,4 +1,8 @@
 (define empty? (lambda (x) (eq? x '())))
 (define length (lambda (ls)
                  (if (empty? ls) 0
-                   (+ 1 (length (cdr ls))))) 
+                   (+ 1 (length (cdr ls))))))
+(define fib (lambda (n)
+              (if (= n 0) 0
+                (if (= n 1) 1
+                  (+ (fib (- n 1)) (fib (- n 2)))))))
